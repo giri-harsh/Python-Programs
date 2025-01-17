81a23c6bd645bdfd7f36d326090d688a331bd9e9
Harsh Giri <2006.harshgiri@gmail.com>
2025-01-18 00:02:28 +0530

    012

---
+++ logical op.py
@@ -0,0 +1,38 @@
+#logical op
+
+
+# NOT OP ---> GIVES OPPOSITE VALUE 
+# TRUE KO FALSE 
+# AND FALSE KO TRUE
+
+print(not True)
+print(not False)
+
+a = 10
+b=5
+print(not(a==b))#true
+
+# AND OPERATOR
+# ---> DONO SAHI HONGE TABHI SAHI AAYEGAA
+# DONO FALSE HONGE TABHI FLAS3E AAYEGA
+
+
+n=True
+m=False
+print(n and m) #false
+
+
+n1=True
+n2=True
+
+print(n1 and n2) #true
+
+
+
+
+# OR OPERATOR
+
+# AGAR EK BHI TRUE TOH TRUE AAYEGA
+
+print(n or m) #true
+
