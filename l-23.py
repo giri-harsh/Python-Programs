7e15ed7f25252ad08615addf0d2a9bc2ea6fd263
Harsh Giri <2006.harshgiri@gmail.com>
2025-05-10 18:35:37 +0530

    Auto commit: l-23.py - 2025-05-10 18:35:37

---
+++ l-23.py
@@ -0,0 +1,13 @@
+class character:
+    # x_pos
+    # health
+    # name 
+    
+def __init__ (self,name,x_pos,health):
+    self.name = name
+    self.x_pos = x_pos 
+    self.health = health
+
+def move(self,by_amount):
+    self.x_pos += by_amount
+    
