6a4cb55fb00b4fc679dcbb8329804fcc9f5f2513
Harsh Giri <2006.harshgiri@gmail.com>
2025-05-11 09:24:44 +0530

    Auto commit: L-13 Ranges.py - 2025-05-11 09:24:44

---
+++ L-13 Ranges.py
@@ -0,0 +1,14 @@
+ten=range(10)
+print(ten[9])
+
+
+ten = range(1,11)
+reversed_ten=reversed(ten)
+print(reversed_ten)
+
+print(12 in ten)
+print(5 not in ten)
+
+
+ten_list = list(ten)
+print(ten_list)
