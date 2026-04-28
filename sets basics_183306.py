67abee37de1caddc718288e1f0e19677955fe70f
Harsh Giri <2006.harshgiri@gmail.com>
2026-04-28 18:33:06 +0530

    adding sets basics_183306.py [2026-04-28 18:33]

---
+++ sets basics_183306.py
@@ -0,0 +1,10 @@
+collection = set( {1,2,3,4,5,6,6,6,6})
+print(collection)
+print(type(collection))
+print(len(collection))
+
+# print("Empty set")
+# set = set()
+# print(len(set))
+# print(type(set))
+# print(set)
