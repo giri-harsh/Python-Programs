1ac5d3e9b1b88b5f2cdb622fe0d07ed13a2711aa
Harsh Giri <2006.harshgiri@gmail.com>
2026-05-02 04:27:07 +0530

    adding traffic light_042707.py [2026-05-02 04:27]

---
+++ traffic light_042707.py
@@ -0,0 +1,13 @@
+light = "blue"
+
+if(light=="red"):
+    print("Stop")
+elif(light=="green"):
+    print("GO")
+
+elif(light=="yellow"):
+    print("Wait")
+
+else :
+    print("none")
+print("end of code")
