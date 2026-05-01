bb2370dba56a8eaf16e4eb21a9e5e24d5f2b4de2
Harsh Giri <2006.harshgiri@gmail.com>
2026-05-01 18:18:03 +0530

    adding greatest of three_181803.py [2026-05-01 18:18]

---
+++ greatest of three_181803.py
@@ -0,0 +1,10 @@
+a = int(input())
+b= int(input())
+c= int(input())
+if (a>b and a>c):
+    print("A is greatest",a)
+elif(b>a and b>c):
+    print("B is greatest")
+elif (c>b and c>a):
+    print("C is greatest")
+   
