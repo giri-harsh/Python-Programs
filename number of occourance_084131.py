fce41b0d9443a5624bb86ae9bda4bb68bf261e18
Harsh Giri <2006.harshgiri@gmail.com>
2026-05-04 08:41:31 +0530

    adding number of occourance_084131.py [2026-05-04 08:41]

---
+++ number of occourance_084131.py
@@ -0,0 +1,3 @@
+str = "Hi my name is Harsh giri I am a 1st year student I study in AKGEC and i am in CSE DS Branch DS is Data science and i am currently studying Python for a great internship opportunity!!!"
+print("Number of times 's' has come is : ",str.count("s"))
+print("Number of times 'S' has come is : ",str.count("S"))
